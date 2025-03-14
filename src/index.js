0d91e27502910096cbc49abbdbb46620e7582837
cha-law <char_law@outlook.com>
2025-03-14 18:06:13 +0000

    Fixed tasks not adding title and time text.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import "./styles.css";
 
+// SVGs
+
+import circleoutline from "./assets/svgs/circle-outline.svg";
+import trashcan from "./assets/svgs/trash-can.svg";
+
 function TodoController() {
+    const Screen = ScreenController();
+
     class Task {
         constructor(title, time, priority) {
             this.title = title;
@@ -16,7 +23,19 @@ function TodoController() {
             } else {
                 checked = false;
                 // logic to change task on screen
-            }
+            };
+        };
+
+        get Title() {
+            return this.title;
+        };
+
+        get Time() {
+            return this.time;
+        };
+
+        get Priority() {
+            return this.priority;
         };
     };
 
@@ -25,8 +44,14 @@ function TodoController() {
     };
 
     function createTask(title, time, priority) {
+        if (localStorage.getItem(title)) {
+            return "DUPLICATE";
+        };
+
         let newTask = new Task(title, time, priority);
-        localStorage.setItem("task", JSON.stringify(newTask)); // needs to not override if same name
+        localStorage.setItem(String(title), JSON.stringify(newTask));
+        Screen.addTaskToScreen(newTask);
+        return newTask;
     };
 
     return { createProject, createTask };
@@ -48,7 +73,57 @@ function ScreenController() {
         content.remove();
     };
 
-    return { removeContents };
+    function addTaskToScreen(taskObject) {
+        let taskDiv = document.createElement("div");
+        taskDiv.classList.toggle("task");
+
+        let leftDiv = document.createElement("div");
+        leftDiv.classList.toggle("left");
+
+        // Adding toggle button and image
+        let toggleButton = document.createElement("button");
+        let toggleButtonImg = document.createElement("img");
+        toggleButton.classList.toggle("toggle");
+        toggleButtonImg.src = circleoutline;
+        toggleButton.appendChild(toggleButtonImg);
+        leftDiv.appendChild(toggleButton);
+
+        // Div containing task information
+        let infoDiv = document.createElement("div");
+        infoDiv.classList.toggle("task-info");
+        let titleText = document.createElement("p");
+        titleText.textContent = taskObject.Title; 
+        let timeText = document.createElement("p");
+        timeText.classList.toggle("time");
+        timeText.textContent = taskObject.Time;
+        infoDiv.appendChild(titleText);
+        infoDiv.appendChild(timeText);
+
+        // Append leftdiv to main div
+        leftDiv.appendChild(infoDiv);
+        taskDiv.appendChild(leftDiv);
+
+        // Adding task option buttons
+        let taskOptionsDiv = document.createElement("div");
+        taskOptionsDiv.classList.toggle("task-options");
+
+        let trashButton = document.createElement("button");
+        trashButton.classList.toggle("delete");
+        
+        let trashButtonImg = document.createElement("img");
+        trashButtonImg.src = trashcan;
+
+        trashButton.appendChild(trashButtonImg);
+        taskOptionsDiv.appendChild(trashButton);
+        taskDiv.appendChild(taskOptionsDiv);
+
+        // Finally, add the task div to the webpage
+        const projects = document.querySelector(".my-projects");
+        projects.appendChild(taskDiv);
+
+    }
+
+    return { removeContents, addTaskToScreen };
 };
 
-TodoController().createTask("eat breakfast", 1130, "high")
+const newtask = TodoController().createTask("bun you fam", "1135", "high")
